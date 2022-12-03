9d470107404b322ee65a9561bc694ea08bd7b05a
RfJH <1024080986@qq.com>
2022-12-03 20:10:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         easydo
+// @namespace    http://
+// @version      0.2
+// @description  方便人类使用
+// @author       RF_JH
+// @match        *://*
+// @license      AGPL-3.0
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+    'use strict';
+
+    // Your code here...
+    console.log("hello world");
+    
+    }
+)();
